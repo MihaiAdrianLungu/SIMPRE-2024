ae3b9d2b9e30438957d972b2ca859106c9fa7919
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2024-04-23 19:29:22 +0300

    added update page for records

--- components/RecordForm.jsx
+++ components/RecordForm.jsx
@@ -27,6 +27,7 @@ const RecordForm = (props) => {
           <input
             type="name"
             id="name"
+            value={data.name}
             onChange={(e) => handleChange("name", e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Name placeholder"
@@ -43,6 +44,7 @@ const RecordForm = (props) => {
           <textarea
             id="description"
             rows="4"
+            value={data.description}
             onChange={(e) => handleChange("description", e.target.value)}
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Write your thoughts here..."
@@ -61,7 +63,7 @@ const RecordForm = (props) => {
             onClick={() => onSubmit(data)}
             className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           >
-            Create
+            {entry?._id ? "Update" : "Create"}
           </button>
         </div>
       </div>
--- utils/recordsFunctions.js
+++ utils/recordsFunctions.js
@@ -1,30 +1,62 @@
 export const getRecords = async () => {
-    try {
-        const response = await fetch('/api/records', {
-            method: 'GET',
-        })
+  try {
+    const response = await fetch("/api/records", {
+      method: "GET",
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if(!data) {
-            return [];
-        }
-
-        return data;
-    } catch (error) {
-        console.log(error)
+    if (!data) {
+      return [];
     }
-}
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecord = async (id) => {
+  try {
+    const response = await fetch(`/api/records?id=${id}`, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const createRecord = async (entry) => {
+  try {
+    const response = await fetch("/api/records", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(entry),
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateRecord = async (entry) => {
     try {
         const response = await fetch('/api/records', {
-            method: 'POST',
+            method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(entry)
-        })
+        });
 
         const data = await response.json();
 
@@ -35,15 +67,15 @@ export const createRecord = async (entry) => {
 }
 
 export const deleteRecord = async (id) => {
-    try {
-        const response = await fetch(`/api/records?id=${id}`, {
-            method: 'DELETE',
-        })
+  try {
+    const response = await fetch(`/api/records?id=${id}`, {
+      method: "DELETE",
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        return data;
-    } catch (error) {
-        console.log(error)
-    }
-}
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
